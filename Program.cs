80adebb8c00597cf101ef8a93d208216a29f3b1e
henrysopo <hgsopog@gmail.com>
2023-05-12 06:11:57 -0500

    Codigo del punto 17

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolucionPunto17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 2;
+            int resultDivision = Division(a, b);
+            //Muestra por consola el valor que devolvio la función
+            Console.Write(a + " dividido " + b + " = " + resultDivision);
+            Console.ReadKey();
+        }
+
+        //Función que recibe 2 enteros y devuelve el resultado de la división entre ellos
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
